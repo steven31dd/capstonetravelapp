1e2059d44018f47ce38dd2b85a39b357b773a7cf
Steven Andrade <stevenandrade45@gmail.com>
2020-07-18 12:05:39 -0700

    node-fetch

--- src/client/js/__test__/nameChecker.test.js
+++
@@ -1,9 +0,0 @@
-import { checkForName } from "../nameChecker"
-
-describe("Testing the checkForName functionality", () => {
-      test("checkForName defined?", () => {
-           expect(checkForName).toBeDefined();
-})});
-
-
-checkForName
--- src/server/index.js
+++ src/server/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const dotenv = require('dotenv');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const fetch = require('node-fetch');
 const app = express()
 
 
@@ -26,12 +27,14 @@ app.get('/', function (req, res) {
 
 // designates what port the app will listen to for incoming requests
 app.listen(8080, function () {
-    console.log('Example app listening on port 8080!')
+  const ServerStart = new Date();
+    console.log(`${ServerStart.getDate()} : @${ServerStart.getHours()} (Military)`);
+    console.log('Server listening on port 8080!')
 })
 
 app.get('/getGeographics', (req, res) => {
   const url = `http://api.geonames.org/postalCodeSearchJSON?placename=${req.query.city}&username=${process.env.Geo_User}`;
-    get(url).then(response => {
+    fetch(url).then(response => {
       res.send(JSON.stringify(response.data.geonames[0]));
     })
     .catch(error => {
@@ -41,7 +44,7 @@ app.get('/getGeographics', (req, res) => {
 
 app.get('/getWeather', (req, res) => {
   const url = `https://api.weatherbit.io/v2.0/current?lat=${req.query.lat}&long=${req.query.long}&key=${process.env.WBit_Key}`;
-    get(url).then(response =>{
+    fetch(url).then(response =>{
       res.send(JSON.stringify(response.data))
     })
     .catch(error => {
@@ -51,7 +54,7 @@ app.get('/getWeather', (req, res) => {
 
 app.get('/getImage', (req, res) => {
   const url = `https://pixabay.com/api/?key=${process.env.pBay_Key}&q=${req.query.img}&image_type=photo`;
-    axios.get(url).then(response =>{
+    fetch.get(url).then(response =>{
       res.send(JSON.stringify(response.data.hits[0]));
     })
     .catch(error => {
